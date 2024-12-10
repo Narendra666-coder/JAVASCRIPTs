6a66ee228e29aa548c6252956409a8e5505c41c5
Narendra666-coder <parmar.narendra.26939465@gmail.com>
2024-12-11 01:27:26 +0530

    variables

---
+++ script.js
@@ -0,0 +1,11 @@
+const accountId=452341;
+let accountEmail="Narendra@123gmail.com";
+var accountPassword="12345";
+accountCity="Bhopal";
+// accountId=2345; //It's not allowed
+accountEmail="npsp@gmail.com";
+accountPassword=12345;
+accountCity="Hyderabad";
+let accountProfile; // it's created undefined because no initalization
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountProfile])
